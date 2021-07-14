f78ee4b1496a9559eb8608804ccfd51d7ad9d7c3
szbani <74052564+szbani@users.noreply.github.com>
2021-07-14 18:28:26 +0200

    Termékek
    -termekek
    -navbar

---
+++ csoport/termek1.php
@@ -0,0 +1,8 @@
+<?php
+include_once "../parts/head.html";
+include_once "../parts/navbar.html";
+
+include_once "../parts/termek_main.html";
+
+include_once "../parts/footer.html";
+?>
--- index.php
+++ index.php
@@ -1,38 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Weblap_cím</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<?php 
+  include_once "parts/head.html";
+  include_once "parts/navbar.html";
     
-    <!-- Bootstrap icons-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+  include_once "parts/index_slideshow.html";
+  include_once "parts/index_main.html";
 
-    <!-- Bootstrap core CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    
-    <!-- CSS -->    
-    <link rel="stylesheet" href="assets/navbar.css">
-    <link rel="stylesheet" href="assets/index.css">
-
-</head>
-<body>
+  include_once "parts/footer.html"
   
+?>
     
-      <?php include_once "parts/navbar.html"?>
-    <main>
-      <?php 
-      include "parts/slideshow.html";
-      include "parts/main.html"?>
-    </main>
-
-    <?php include_once "parts/footer.html"?>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script> 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-
-<script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
-</body>
-</html>

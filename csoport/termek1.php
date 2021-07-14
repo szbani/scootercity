cd9a618dfae2865c6c4e415b4c1148ad680ec01b
szbani <74052564+szbani@users.noreply.github.com>
2021-07-15 00:24:51 +0200

    sidebar
    -sidebar

--- csoport/termek1.php
+++ csoport/termek1.php
@@ -1,8 +1,13 @@
 <?php
-include_once "../parts/head.html";
+include_once "../parts/head_start.html";
+?>
+    <link rel="stylesheet" href="/scootercity/assets/sidebar.css">
+<?php
+include_once "../parts/head_end.html";
 include_once "../parts/navbar.html";
 
 include_once "../parts/termek_main.html";
+include_once "../parts/termek_sidebar.html";
 
 include_once "../parts/footer.html";
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
-  include_once "parts/head.html";
+  include_once "parts/head_start.html";
+  include_once "parts/head_end.html";
   include_once "parts/navbar.html";
     
   include_once "parts/index_slideshow.html";

6d5ea181491b740623ce12eea85919b0570aa1a6
szbani <74052564+szbani@users.noreply.github.com>
2021-07-21 22:17:05 +0200

    termékek
    +termékek

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
   include_once "parts/head_start.html";
   include_once "parts/head_end.html";
   include_once "parts/navbar.html";
-    
+    //main
   include_once "parts/index_slideshow.html";
   include_once "parts/index_main.html";
-
+    //footer
   include_once "parts/footer.html"
   
 ?>

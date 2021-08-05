c8b2892cfb3b8e36fc549515730e978b93de02cc
szbani <74052564+szbani@users.noreply.github.com>
2021-08-05 21:32:54 +0200

    design

--- assets/termek.js
+++ assets/termek.js
@@ -1,3 +1,8 @@
+$(document).ready(function () {
+    $('#navbar').removeClass('sticky-top');
+    $('#navbar').addClass('fixed-top');
+});
+
 function modal_show(clicked){
     $('#Termek_Neve').text($("#"+clicked.id+" #Param_Nev").text());
     

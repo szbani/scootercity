284b7acf63b204cc4c136937e8c30346780d34d5
szbani <74052564+szbani@users.noreply.github.com>
2021-08-07 21:37:47 +0200

    termek modal

--- assets/termek.js
+++ assets/termek.js
@@ -5,7 +5,9 @@ $(document).ready(function () {
 
 function modal_show(clicked){
     $('#Termek_Neve').text($("#"+clicked.id+" #Param_Nev").text());
+    $('#Termek_Ara').text($('#'+clicked.id+' #Param_Ar').text());
     
+    $('#Termek_Raktar').attr('src', $('#'+clicked.id+' #Param_Raktar').attr('src'));
 }
 
 //#termek_modal

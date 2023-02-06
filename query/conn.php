c403521dff29afd262e7b61f8e275e4ef0c58043
szabó dani <szabodani2001@gmail.com>
2023-02-06 14:12:40 +0100

    0.83b

--- query/conn.php
+++ query/conn.php
@@ -11,7 +11,52 @@ $conn = new mysqli($host,$username,$password,$dbname,$port);
     die("Connection failed: " . mysqli_connect_error());
   }
 
+class dataBase{
+  private $connection = null;
+  public function __construct($dbhost= 'localhost',$dbname='scootercity',$dbuser='root',$dbpass='',$charset='utf8')
+  { 
+    try{
+      $this->connection = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+      $this->connection->set_charset($charset);
+      if(mysqli_connect_errno()){
+        throw new Exception('Sikertelen kapcsolódás az adatbázishoz!');
+      }
+    }catch(Exception $e){
+      throw new Exception($e->getMessage());
+    }
+  }
+  
+  private function executeStatement( $query = '',$params=[]){
+    try{
+      $stmnt = $this->connection->prepare($query);
+      if($stmnt === false){
+        throw new Exception("nem lehet végrehajtani a parancsot: " .$query);
+      }
+      if($params){
+        call_user_func_array(array($stmnt, 'bind_param'),$params);
+      }
+      $stmnt->execute();
+      return $stmnt;
+    }catch(Exception $e){
+      throw new Exception($e->getMessage());
+    }
 
+  }
+
+  public function Select($query = '',$params = []){
+    try{
+      $stmnt = $this->executeStatement($query,$params);
+      $result = $stmnt->get_result()->fetch_all(MYSQLI_ASSOC);
+      $stmnt->close();
+      
+      return $result;
+    }catch(Exception $e){
+      throw new Exception($e->getMessage());
+    }
+    return false;
+  }
+
+}
 
 
 ?>
--- termek.php
+++ termek.php
@@ -1,6 +1,11 @@
 <?php
 require_once 'query/conn.php';
 $url = explode("/",$_SERVER['REQUEST_URI']);
+$db = new dataBase();
+$termek = $db->Select("SELECT t.*,k.nev as knev, (SELECT GROUP_CONCAT(file_name ORDER BY img_order) FROM kepek k WHERE k.termek_id = t.id) as images FROM `termekek` t 
+INNER JOIN `kategoriak` k ON k.id = t.kategoria WHERE t.id = '$url[3]';");
+$termek = $termek[0];
+$tul = json_decode($termek['tulajdonsagok']);
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,6 +15,23 @@ require_once "parts/head.php";
 <body>
 <?php
 require_once "parts/navbar.php";
+?>
+<div class="row">
+    <div class="col-6">
+
+    </div>
+    <div class="col-6">
+        <p><?php echo $termek['nev']?></p>
+        <p><?php echo $termek['ar']?></p>
+        <p><?php echo $termek['kategoria']?></p>
+        <p><?php echo $termek['leiras']?></p>
+        <?php
+        // var_dump($url);
+        var_dump($tul);echo'<br>';
+         var_dump($termek);?>
+    </div>
+</div>
+<?php
 require_once "parts/footer.php";
 ?>
 </body>
